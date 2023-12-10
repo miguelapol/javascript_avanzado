142bf39042b70218dce938ab5a802dcd9231621d
Miguel Angel <33709574+miguelapol@users.noreply.github.com>
2023-12-10 14:45:36 -0600

    cambio de variables

---
+++ app3.js
@@ -0,0 +1,6 @@
+"use strict";
+//con esto podemos cambiar el valor de variables de maner inversa
+let nombreCompleto = 'Juan';
+let apellidoCompleto = "apolonio";
+[nombreCompleto, apellidoCompleto] = [apellidoCompleto, nombreCompleto];
+console.log(nombreCompleto, apellidoCompleto);
---
+++ app3.ts
@@ -0,0 +1,8 @@
+
+//con esto podemos cambiar el valor de variables de maner inversa
+let nombreCompleto='Juan';
+let apellidoCompleto="apolonio";
+
+[nombreCompleto,apellidoCompleto]=[apellidoCompleto,nombreCompleto];
+
+console.log(nombreCompleto,apellidoCompleto);
